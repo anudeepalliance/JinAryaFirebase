b3a6a97bcae9394e3ec397d67df3135fe71cd5f7
Anudeep Ananth <anudeepalliance>
2018-07-06 07:33:08 +0530

    1st commit

--- app/src/main/java/com/example/work/jinaryafirebase/MainActivity.kt
+++ app/src/main/java/com/example/work/jinaryafirebase/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //successful feature added
+        //successful 2nd feature added
     }
 }

d421d507356f36c3d8962e8758c1db78ca4fd50d
Anudeep Ananth <anudeepalliance>
2018-07-06 06:30:04 +0530

    1st commit

--- app/src/main/java/com/example/work/jinaryafirebase/MainActivity.kt
+++ app/src/main/java/com/example/work/jinaryafirebase/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //comment added again
+        //comment added again and again
     }
 }
